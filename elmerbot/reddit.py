12e3d9c7215573b8c1cbbf1656810ba132b830c6
Scott Hand <scott@vkgfx.com>
2017-03-14 00:29:20 -0500

    Adds more error handling to prevent PRAW driven crashes

--- elmerbot/reddit.py
+++ elmerbot/reddit.py
@@ -23,31 +23,35 @@ class RedditFeed(object):
 
     async def _handle_submission(self, submission):
         # Sleep for 5 minutes to give OP time to make the review comment
-        print("Handling {}".format(submission.title))
-        await asyncio.sleep(300.0)
-        output = []
-        output.append("https://www.reddit.com{}\n".format(submission.permalink))
-        # Search for oldest comment from author. There should be a way to do this directly with PRAW, but I don't see
-        # it, so just doing a linear search for now...
-        submission.comments.replace_more(limit=0)
-        oldest = None
-        review_comment = None
-        for comment in submission.comments.list():
-            if comment.author == submission.author:
-                if not oldest or comment.created < oldest:
-                    oldest = comment.created
-                    review_comment = comment
-        if review_comment:
-            blurb = review_comment.body if len(review_comment.body) <= 400 else review_comment.body[:400] + "..."
-            output.append(blurb)
+        try:
+            print("Handling {}".format(submission.title))
+            await asyncio.sleep(300.0)
+            output = []
+            output.append("https://www.reddit.com{}\n".format(submission.permalink))
+            # Search for oldest comment from author. There should be a way to do this directly with PRAW, but I don't
+            # see it, so just doing a linear search for now...
+            submission.comments.replace_more(limit=0)
+            oldest = None
+            review_comment = None
+            for comment in submission.comments.list():
+                if comment.author == submission.author:
+                    if not oldest or comment.created < oldest:
+                        oldest = comment.created
+                        review_comment = comment
+            if review_comment:
+                blurb = review_comment.body if len(review_comment.body) <= 400 else review_comment.body[:400] + "..."
+                output.append(blurb)
 
-        em = discord.Embed(title="**New Review from u/{}: {}**".format(submission.author.name, submission.title),
-                           description="\n".join(output),
-                           colour=0x00DD00)
-        em.set_thumbnail(url=submission.url)
+            em = discord.Embed(title="**New Review from u/{}: {}**".format(submission.author.name, submission.title),
+                               description="\n".join(output),
+                               colour=0x00DD00)
+            em.set_thumbnail(url=submission.url)
 
-        for channel in self._channels:
-            await self._client.send_message(channel, embed=em)
+            for channel in self._channels:
+                await self._client.send_message(channel, embed=em)
+        except Exception as e:
+            print("Got exception: {}".format(e))
+            await asyncio.sleep(0)
 
     async def _check_submissions(self):
         try:
